a6cc0294c4a37defd93dbb2f80b8f56fed4c8700
Joe Karasek and Mary Warrington <marywarrington@gmail.com>
2016-03-02 10:13:29 -0800

    Add getters, setters, save, getall, deleteall to patrons.php

---
+++ src/Patron.php
@@ -0,0 +1,92 @@
+<?php
+    class Patron
+    {
+        private $name;
+        private $email;
+        private $id;
+
+        function __construct($name, $email, $id = null)
+        {
+            $this->name = $name;
+            $this->email = $email;
+            $this->id = $id;
+        }
+
+        function getName()
+        {
+            return $this->name;
+        }
+
+        function setName($new_name)
+        {
+            $this->name = $new_name;
+        }
+
+        function getEmail()
+        {
+            return $this->email;
+        }
+
+        function setEmail($new_email)
+        {
+            $this->email = $new_email;
+        }
+
+        function getId()
+        {
+            return $this->id;
+        }
+
+        function save()
+        {
+              $GLOBALS['DB']->exec("INSERT INTO patrons (name, email) VALUES ('{$this->getName()}', '{$this->getEmail()}');");
+              $this->id = $GLOBALS['DB']->lastInsertId();
+        }
+
+        static function getAll()
+        {
+            $returned_patrons = $GLOBALS['DB']->query("SELECT * FROM patrons;");
+            $patrons = array();
+            foreach($returned_patrons as $patron) {
+                $name = $patron['name'];
+                $email = $patron['email'];
+                $id = $patron['id'];
+                $new_patron = new Patron($name, $email, $id);
+                array_push($patrons, $new_patron);
+            }
+            return $patrons;
+        }
+
+        static function findById($search_id)
+        {
+            $found_patron = null;
+            $patrons = Patron::getAll();
+            foreach($patrons as $patron) {
+                $patron_id = $patron->getId();
+                if ($patron_id == $search_id) {
+                  $found_patron = $patron;
+                }
+            }
+            return $found_patron;
+        }
+
+        static function findByName($search_name)
+        {
+            $found_patron = null;
+            $patrons = Patron::getAll();
+            foreach($patrons as $patron) {
+                $patron_name = $patron->getName();
+                if ($patron_name == $search_name) {
+                  $found_patron = $patron;
+                }
+            }
+            return $found_patron;
+        }
+
+        static function deleteAll()
+        {
+            $GLOBALS['DB']->exec('DELETE FROM patrons;');
+        }
+
+    }
+?>
---
+++ tests/PatronTest.php
@@ -0,0 +1,247 @@
+<?php
+
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+
+    require_once __DIR__ . '/../src/Patron.php';
+
+    $server = 'mysql:host=localhost;dbname=library_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    class PatronTest extends PHPUnit_Framework_TestCase
+    {
+        protected function tearDown()
+        {
+            Patron::deleteAll();
+        }
+
+        function testInfo()
+        {
+            //Arrange
+            $name = "Harry Houdini";
+            $email = "harry@houdini.com";
+            $id = null;
+            $test_patron = new Patron($name, $email, $id);
+
+            //Act
+            $result1 = $test_patron->getName();
+            $result2 = $test_patron->getId();
+            $result3 = $test_patron->getEmail();
+
+            //Assert
+            $this->assertEquals($name, $result1);
+            $this->assertEquals($id, $result2);
+            $this->assertEquals($email, $result3);
+        }
+
+        function testSave()
+        {
+            //Arrange
+            $name = "Harry Houdini";
+            $email = "harry@houdini.com";
+            $id = null;
+            $test_patron = new Patron($name, $email, $id);
+
+            //Act
+            $test_patron->save();
+
+            //Assert
+            $result = Patron::getAll();
+            $this->assertEquals($test_patron, $result[0]);
+        }
+
+        function testGetAll()
+        {
+            //Arrange
+            $name = "Harry Houdini";
+            $email = "harry@houdini.com";
+            $id = null;
+            $test_patron = new Patron($name, $email, $id);
+            $test_patron->save();
+
+            $name2 = "Maggie Pie";
+            $email2 = "maggie@pie.com";
+            $test_patron2 = new Patron($name2, $email2, $id);
+            $test_patron2->save();
+
+            //Act
+            $result = Patron::getAll();
+
+            //Assert
+            $this->assertEquals([$test_patron, $test_patron2], $result);
+        }
+        //
+        function testDeleteAll()
+        {
+            //Arrange
+            $name = "Harry Houdini";
+            $email = "harry@houdini.com";
+            $id = null;
+            $test_patron = new Patron($name, $email, $id);
+            $test_patron->save();
+
+
+            $name2 = "Maggie Pie";
+            $email2 = "maggie@pie.com";
+            $test_patron2 = new Patron($name2, $email2, $id);
+            $test_patron2->save();
+
+            //Act
+            Patron::deleteAll();
+
+            //Assert
+            $result = Patron::getAll();
+            $this->assertEquals([], $result);
+        }
+
+        function testFindById()
+        {
+            //Arrange
+            $name = "Harry Houdini";
+            $email = "harry@houdini.com";
+            $id = null;
+            $test_patron = new Patron($name, $email, $id);
+            $test_patron->save();
+
+
+            $name2 = "Maggie Pie";
+            $email2 = "maggie@pie.com";
+            $test_patron2 = new Patron($name2, $email2, $id);
+            $test_patron2->save();
+
+            //Act
+            $result = Patron::findById($test_patron->getId());
+
+            //Assert
+            $this->assertEquals($test_patron, $result);
+        }
+
+        function testFindByName()
+        {
+            //Arrange
+            $name = "Harry Houdini";
+            $email = "harry@houdini.com";
+            $id = null;
+            $test_patron = new Patron($name, $email, $id);
+            $test_patron->save();
+
+
+            $name2 = "Maggie Pie";
+            $email2 = "maggie@pie.com";
+            $test_patron2 = new Patron($name2, $email2, $id);
+            $test_patron2->save();
+
+            //Act
+            $result = Patron::findByName($test_patron->getName());
+
+            //Assert
+            $this->assertEquals($test_patron, $result);
+        }
+
+
+        // function testDeletePatron()
+        // {
+        //     //Arrange
+        //     $name = "Harry Houdini";
+        //
+        //     $id = 1;
+        //     $test_patron = new Patron($name, $enrollment_date, $id);
+        //     $test_patron->save();
+        //
+        //
+        //     $name2 = "Maggie Pie";
+        //     $enrollment_date2 = '2016-03-05';
+        //     $id2 = 2;
+        //     $test_patron2 = new Patron($name2, $enrollment_date2, $id2);
+        //     $test_patron2->save();
+        //
+        //
+        //     //Act
+        //     $test_patron->delete();
+        //
+        //     //Assert
+        //     $this->assertEquals(Patron::getAll(), [$test_patron2]);
+        // }
+        //
+        //
+        // function testAddCourse()
+        // {
+        //     //Arrange
+        //     $subject = "Chemistry 101";
+        //     $course_number = 'CHEM101';
+        //     $id = 1;
+        //     $test_course = new Course($subject, $course_number, $id);
+        //     $test_course->save();
+        //
+        //     $name = "Harry Houdini";
+        //
+        //     $id = 1;
+        //     $test_patron = new Patron($name, $enrollment_date, $id);
+        //     $test_patron->save();
+        //
+        //     //Act
+        //     $test_patron->addCourse($test_course);
+        //
+        //     //Assert
+        //     $this->assertEquals($test_patron->getCourses(), [$test_course]);
+        // }
+        //
+        // function testGetCourses()
+        // {
+        //     //Arrange
+        //     $name = "Harry Houdini";
+        //
+        //     $id = 1;
+        //     $test_patron = new Patron($name, $enrollment_date, $id);
+        //     $test_patron->save();
+        //
+        //
+        //     $subject2 = "Chemistry 101";
+        //     $course_number = 'CHEM101';
+        //     $id2 = 1;
+        //     $test_course = new Course($subject2, $course_number, $id2);
+        //     $test_course->save();
+        //
+        //     $subject3 = "Chemistry 103";
+        //     $course_number2 = 'CHEM103';
+        //     $id3 = 2;
+        //     $test_course2 = new Course($subject3, $course_number, $id3);
+        //     $test_course2->save();
+        //
+        //     //Act
+        //     $test_patron->addCourse($test_course);
+        //     $test_patron->addCourse($test_course2);
+        //
+        //     //Assert
+        //     $this->assertEquals($test_patron->getCourses(), [$test_course, $test_course2]);
+        // }
+        //
+        // function testDelete()
+        // {
+        //     //Arrange
+        //     $subject = "Math";
+        //     $course_number = "MTH101";
+        //     $id = 1;
+        //     $test_course = new Course($subject, $course_number, $id);
+        //     $test_course->save();
+        //
+        //     $name = "Maggie Pie";
+        //     $enrollment_date = '2016-03-05';
+        //     $id2 = 2;
+        //     $test_patron = new Patron($name, $enrollment_date, $id2);
+        //     $test_patron->save();
+        //
+        //     //Act
+        //     $test_patron->addCourse($test_course);
+        //     $test_patron->delete();
+        //
+        //     //Assert
+        //     $this->assertEquals([], $test_course->getPatrons());
+        // }
+
+    }
+ ?>
